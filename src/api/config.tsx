d965ef01f00a8619309280aef89bdf262840dc6e
AnggaWikaNugraha <anggawika18@gmail.com>
2022-01-25 08:38:36 +0700

    update mobile

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 let AXIOSBASE :any  = {}
 
 AXIOSBASE.STAGING = axios.create({
-  baseURL : `http://staging-cuan.awalmula.co/rest/default/V1`
+  baseURL : `https://staging-cuan.awalmula.co/rest/default/V1`
 })
 
 export default AXIOSBASE;
